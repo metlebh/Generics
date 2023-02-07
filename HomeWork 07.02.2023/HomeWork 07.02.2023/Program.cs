3980eba7e5dae654085ee866740c901a6f2ef655
metlebh <121571075+metlebh@users.noreply.github.com>
2023-02-07 18:28:22 +0400

    Add files via upload
    InseAllah Duz Olar

---
+++ HomeWork 07.02.2023/HomeWork 07.02.2023/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HomeWork_07._02._2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Swap<string>("salam", "sagol");
+            GetType(5);
+
+
+
+        }
+
+
+        public static void GetType<T>(T value)
+        {
+            Console.WriteLine(value);
+        }
+
+
+        static void Swap<T>(T a, T b)
+        {
+            T c = default;
+            T temporary = c;
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"Swapped values:{a} , {b}.");
+        }
+
+
+
+
+
+
+    }
+}
